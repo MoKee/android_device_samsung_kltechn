1439a7a1c4a9b45cf115265812c5c6f20217b7e6
martincz <martincz@mokeedev.com>
2015-05-27 18:02:09 +0800

    mokee bringup

--- cm.mk
+++
@@ -1,8 +0,0 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-$(call inherit-product, device/samsung/kltechn/full_kltechn.mk)
-
-PRODUCT_DEVICE := kltechn
-PRODUCT_NAME := cm_kltechn
