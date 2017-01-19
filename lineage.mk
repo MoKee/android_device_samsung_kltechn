3103860db1fb382a85f2f5cfd161972f03908993
martincz <martincz@mokeedev.com>
2017-01-19 14:49:43 +0800

    kltechn: mokee-ify

--- lineage.mk
+++
@@ -1,6 +0,0 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-$(call inherit-product, device/samsung/kltechn/full_kltechn.mk)
-
-PRODUCT_DEVICE := kltechn
-PRODUCT_NAME := lineage_kltechn
