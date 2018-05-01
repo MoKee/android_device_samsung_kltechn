98bb3930a2d8f13281ef3c97f4c6d0d2ae6d6389
Martincz Gao <martincz@mokeedev.com>
2018-05-01 15:40:38 +0800

    kltechn: mokee-ify
    Change-Id: I455e0b95644d414c43bc7a0eab00b7f05f373973

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ include device/samsung/klte-common/BoardConfigCommon.mk
 TARGET_OTA_ASSERT_DEVICE := klte,kltechn,kltexx,kltezm,kltezn
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineage_kltechn_defconfig
+TARGET_KERNEL_CONFIG := mokee_kltechn_defconfig
 
 # Fingerprint
 include $(COMMON_PATH)/fingerprint/board.mk
